c8132e2e7ffe3eee3b5cf0a15811f60a65b9e69d
bradphelan <b.phelan@weingartner.com>
2018-02-15 12:43:05 +0100

    Added bug12783

--- EyeshotBugs/Bug12720.cs
+++ EyeshotBugs/Bug12720.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using devDept.Eyeshot;
 using devDept.Eyeshot.Entities;
@@ -37,10 +38,57 @@ namespace EyeshotBugs
                     // Comment out the below line and the bar
                     // will be visible.
                     blockRef.Transformation = transform;
+                    blockRef.RegenMode = regenType.NotNeeded;
 
+                    //vpl.Entities.Regen(  );
                     await Eyeshot.ViewportLayout.ClosedTask;
 
                 } );
         }
+
+        [Fact]
+        public async Task ShouldWork2()
+        {
+            await Eyeshot.RunSTADesktop
+                ( async () =>
+                {
+                    var vpl = Eyeshot.ViewportLayout.ViewportLayout;
+
+                    var block = new Block("Foo");
+                    var blockRef = new BlockReference( new Identity(), "Foo" );
+
+                    var cube = new Bar( 0,0,0,1,0,0,0.5,20);
+
+                    block.Entities.Add(cube);
+
+                    vpl.Blocks.Add( block );
+                    vpl.Entities.Add(blockRef  );
+                    await Task.Delay( TimeSpan.FromMilliseconds( 100 ) );
+                    vpl.ZoomFit();
+
+                    var angle = 0;
+                    Observable
+                       .Interval( TimeSpan.FromMilliseconds( 10 ) )
+                       .ObserveOnDispatcher()
+                       .Subscribe
+                            ( v =>
+                            {
+                                angle = angle + 5;
+                                var transform = new Rotation( angle * Math.PI/180,Vector3D.AxisZ );
+
+                                // Comment out the below line and the bar
+                                // will be visible.
+                                blockRef.Transformation = transform;
+                                blockRef.RegenMode = regenType.NotNeeded;
+                                vpl.Invalidate();
+
+                            });
+
+                    //vpl.Entities.Regen(  );
+                    await Eyeshot.ViewportLayout.ClosedTask;
+
+                } );
+        }
+
     }
 }
---
+++ EyeshotBugs/Bug12783.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using devDept.Eyeshot;
+using devDept.Eyeshot.Entities;
+using devDept.Geometry;
+using EyeshotBugs.Utils;
+using Xunit;
+
+namespace EyeshotBugs
+{
+    public class Bug12783
+    {
+
+        [Fact]
+        public async Task ShouldWork2()
+        {
+            await Eyeshot.RunSTADesktop
+                ( async () =>
+                {
+                    var vpl = Eyeshot.ViewportLayout.ViewportLayout;
+
+                    var block = new Block("Foo");
+                    var blockRef = new BlockReference( new Identity(), "Foo" );
+
+                    vpl.Blocks.Add( block );
+                    vpl.Entities.Add(blockRef  );
+
+                    var dim = new LinearDim( Plane.XY
+                                             , new Point3D(0,0,0)
+                                            ,  new Point3D(1,0,0)
+                                            , new Point3D(0.5,0.5,0)
+                                            ,0.2  );
+
+                    dim.Color = Color.Blue;
+                    dim.ColorMethod = colorMethodType.byEntity;
+
+                    block.Entities.Add(dim  );
+                    block.RegenAllCurved( new RegenParams( 0.1, vpl ) );
+
+
+
+                    await Eyeshot.ViewportLayout.ClosedTask;
+
+                } );
+        }
+    }
+}
